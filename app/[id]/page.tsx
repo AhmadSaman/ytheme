9f2859be285eb0b8ecf79b4f55bac93977892d3a
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2024-09-01 12:31:33 +0300

    add: test use case

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,7 @@
 async function page({ params }: { params: { id: string } }) {
-  const res = await fetch(`http://localhost:3000/api/video/${params.id}`);
-  const json = await res.json();
-  return <div>{JSON.stringify(json)}</div>;
+  // const res = await fetch(`http://localhost:3000/api/video/${params.id}`);
+  // const json = await res.json();
+  return <div>{params.id}</div>;
 }
 
 export default page;
