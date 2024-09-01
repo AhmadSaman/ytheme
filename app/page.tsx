5048f843b34dc7c5615bc97840d932a6826c0d52
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2024-09-01 12:20:55 +0300

    config: shadcn+tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
-  return <p>youtube chapters</p>;
+  return (
+    <div>
+      <p className="text-purple-600 text-2xl">youtube chapters</p>
+      <Button asChild>
+        <Link href={"jXyTIQOfTTk"}>text link</Link>
+      </Button>
+    </div>
+  );
 }
