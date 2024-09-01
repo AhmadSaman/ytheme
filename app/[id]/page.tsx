d5d68ae54e3769b677824ffed902890cf270260d
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2024-09-01 12:37:17 +0300

    add: error handling

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,28 @@
-async function page({ params }: { params: { id: string } }) {
-  // const res = await fetch(`http://localhost:3000/api/video/${params.id}`);
-  // const json = await res.json();
-  return <div>{params.id}</div>;
+interface PageProps {
+  params: { id: string };
 }
 
-export default page;
+const Page = async ({ params }: PageProps) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/video/${params.id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const json = await res.json();
+    console.log(json);
+
+    return (
+      <div>
+        <h1>Video ID: {params.id}</h1>
+        <pre>{JSON.stringify(json, null, 2)}</pre>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error loading data.</div>;
+  }
+};
+
+export default Page;
