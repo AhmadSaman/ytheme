35cf8abd567307f451c6dc22e9b36777184a1e31
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2024-09-13 10:30:02 +0300

    add(ui): simple chapters

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { hc } from "hono/client";
 import { AppType } from "../api/[...route]/route";
 interface PageProps {
@@ -15,12 +16,14 @@ const Page = async ({ params }: PageProps) => {
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
-    const json = await res.json();
+    const {
+      data: { chapters },
+    } = await res.json();
     return (
-      <div className="lg:flex text-slate-100 h-full">
-        <main className="flex-1 flex lg:h-full h-[300px]">
+      <div className="flex flex-col xl:flex-row gap-4 p-4 text-slate-100 h-full">
+        <main className="flex xl:h-full h-[50%]">
           <iframe
-            className="w-full flex-1 rounded-lg overflow-hidden m-4 box-border"
+            className="flex-1  rounded-lg"
             src={`https://www.youtube.com/embed/${params.id}`}
             title="YouTube video player"
             // frameborder="0"
@@ -29,8 +32,22 @@ const Page = async ({ params }: PageProps) => {
             allowFullScreen
           ></iframe>
         </main>
-        <section className="lg:w-[20%] md:w-[200px] w-full m-4">
-          {/* <pre> {JSON.stringify(json, null, 2)}</pre> */}
+        <section className="2xl:w-[500px] h-full xl:w-[30%]  flex flex-col gap-6 w-full overflow-auto p-4 bg-zinc-900 rounded-md">
+          {chapters?.map((chapter) => (
+            <div className="flex gap-2 items-center">
+              <Image
+                src={chapter.thumbnails[0].url}
+                alt="thumbnail"
+                width={chapter.thumbnails[0].width}
+                height={chapter.thumbnails[0].height}
+                className="rounded-md"
+              />
+              <div className="text-sm">
+                <p>{chapter.title}</p>
+                <p>{chapter.time}</p>
+              </div>
+            </div>
+          ))}
         </section>
       </div>
     );
